d9e2790293cf59d9c0536c0d91857d7fea4f3790
Isaac Adumoah <sduisaac@yahoo.com>
2023-12-13 23:01:53 +0000

    final

--- App.js
+++ App.js
@@ -7,9 +7,9 @@ function App() {
   const Stack = createNativeStackNavigator();
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name='Home' component={HomeScreen}/>
-        <Stack.Screen name='Welcome' component={WelcomeScreen}/>
+      <Stack.Navigator>
+        <Stack.Screen component={HomeScreen}/>
+        <Stack.Screen  component={WelcomeScreen}/>
 
       </Stack.Navigator>
     </NavigationContainer>
